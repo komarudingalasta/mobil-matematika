c164006c98cfa798465654593a4d40f42c3f19b1
komarudingalasta <komarudingalasta@gmail.com>
2026-08-14 22:12:52 +0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDu6ab32aMdDjxvXJM9rLcFsuo5EAFkTSo",
-  authDomain: "math-race-komarudin.firebaseapp.com",
-  databaseURL: "https://math-race-komarudin-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "math-race-komarudin",
-  storageBucket: "math-race-komarudin.firebasestorage.app",
-  messagingSenderId: "981533333094",
-  appId: "1:981533333094:web:719251ad6f5b1d42f03fa7"
-};
